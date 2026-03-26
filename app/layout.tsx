9ba69c645131a8d67ff1acd0270735a262bbf33d
Shubham Sharma <shubhamsh3099@gmail.com>
2026-03-26 08:01:33 +0000

    Add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <nav>
-          
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body className="box-border">
+        <nav className="flex items-center justify-between text-[1.25rem] h-[3.75rem] px-[4rem] border-1 mt-[4rem]">
+          <div className="w-[22.5rem]">
+            <ul className="flex justify-between">
+              <li><a href="/">Home</a></li>
+              <li><a href="#">Blogs</a></li>
+              <li><a href="#">Projects</a></li>
+              <li><a href="/resume.pdf" download>Resume</a></li>
+            </ul>
+          </div>
+          <div className="w-[14.37rem]">
+            <ul className="flex justify-between">
+              <li><a href="https://github.com/Dimension5D" target="_blank">Github</a></li>
+              <li><a href="https://www.linkedin.com/in/shubham-sharma3099/" target="_blank">LinkedIn</a></li>
+              <li><a href="https://x.com/Dimension5D" target="_blank">X</a></li>
+            </ul>
+          </div>
         </nav>
         {children}
       </body>
