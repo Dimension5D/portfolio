3e55bc69ab337fad947b3cf262100606ad084be6
Shubham Sharma <shubhamsh3099@gmail.com>
2026-03-26 17:34:06 +0000

    Make responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="box-border bg-gradient-to-bl from-[#FFECEC] to-[#FFFFFF] h-[100vh] pt-[4rem]">
-        <nav className="flex items-center justify-between text-[1.25rem] h-[3.75rem] px-[4rem] border-1">
+        <nav className="flex items-center justify-center text-[1rem] h-[3.75rem] px-[4rem] border-1 md:text-[1.25rem] md:justify-between">
           <div className="w-[22.5rem]">
             <ul className="flex justify-between">
               <li><a href="/">Home</a></li>
@@ -26,7 +26,7 @@ export default function RootLayout({
               <li><a href="/resume.pdf" download>Resume</a></li>
             </ul>
           </div>
-          <div className="w-[14.37rem]">
+          <div className="w-[14.37rem] hidden md:block">
             <ul className="flex justify-between">
               <li><a href="https://github.com/Dimension5D" target="_blank">Github</a></li>
               <li><a href="https://www.linkedin.com/in/shubham-sharma3099/" target="_blank">LinkedIn</a></li>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-[38rem]">
-      <section className="flex h-[22rem] w-[65rem] justify-between items-center">
-        <div className="w-[34.6rem]">
-          <h1 className="text-[3.6rem] font-[Times_New_Roman]">SHUBHAM SHARMA</h1>
-          <h2 className="mb-[2.2rem]">Full Stack Developer | Bug Bounty Hunter</h2>
-          <p className="text-[1.5rem] lh-[2.6rem]">Full stack developer and bug bounty hunter focused on understanding how systems work — and where they fail.</p>
+    <main className="flex justify-center items-center h-[28rem] md:h-[38rem]">
+      <section className="flex flex-wrap md:flex-nowrap h-[22rem] w-[65rem] justify-between items-center md:flex-row">
+        <div className="w-full text-center md:w-[34.6rem] md:text-left">
+          <h1 className="p-[1.8] text-[9vw] font-[Times_New_Roman] md:text-left md:text-[3.6rem]">SHUBHAM SHARMA</h1>
+          <h2 className="md:mb-[2.2rem] md:text-left">Full Stack Developer | Bug Bounty Hunter</h2>
+          <p className="text-[1.2rem] p-[2rem] text-left lh-[2.6rem] md:text-[1.5rem] md:p-0">Full stack developer and bug bounty hunter focused on understanding how systems work — and where they fail.</p>
         </div>
-        <img src="/photo.png" alt="shubham sharma smiling at the face of camera" className="w-[268px] h-[286px]"/>
+        <div className="w-full px-[2rem] block md:hidden">
+          <ul className="flex w-[9rem] justify-between">
+            <li><a href="https://github.com/Dimension5D" target="_blank">Github</a></li>
+            <li><a href="https://www.linkedin.com/in/shubham-sharma3099/" target="_blank">LinkedIn</a></li>
+            <li><a href="https://x.com/Dimension5D" target="_blank">X</a></li>
+          </ul>
+        </div>
+        <img src="/photo.png" alt="shubham sharma smiling at the face of camera" className="w-[265px] h-[285px] md:w-[268px] md:h-[286px] m-auto mt-[2rem]"/>
       </section>
     </main>
   );
